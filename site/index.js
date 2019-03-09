0d4bc4ff3f4b5a626906aadf7935367bb1d190ec
daiiz <da.iz.mg@gmail.com>
2019-03-10 02:23:09 +0900

    randomなケース

--- site/index.js
+++ site/index.js
@@ -1,8 +1,18 @@
-const js = import("../pkg/hello_rust_wasm.js");
+const jsPromise = import("../pkg/hello_rust_wasm.js");
 
 window.logger = str => { console.log(">", str) }
 
-js.then(js => {
-  js.greet("WebAssembly");
-  console.log(js.gcd(3 * 17 * 23, 3 * 19 * 23));
-});
+// jsPromise.then(js => {
+//   js.greet("WebAssembly");
+//   console.log(js.gcd(3 * 17 * 23, 3 * 19 * 23));
+// });
+
+const main = async () => {
+  window.rs = await jsPromise
+
+  rs.greet("WebAssembly");
+  console.log(rs.gcd(3 * 17 * 23, 3 * 19 * 23));
+  console.log(rs.gcd(Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)));
+}
+
+main()
