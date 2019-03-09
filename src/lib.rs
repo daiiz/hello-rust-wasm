f16673fd5b5048f003f18fc39bb5c43cc0a45b33
daiiz <da.iz.mg@gmail.com>
2019-03-09 21:40:07 +0900

    諸々書いた

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// It allows JavaScript to call a Rust API with a string,
+// or a Rust function to catch a JavaScript exception.
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+// Calling external functions in JavaScript from Rust
+// The attribute says "wasm-bindgen knows how to find these functions".
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+// Producing Rust functions that JavaScript can call
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}", name));
 }
